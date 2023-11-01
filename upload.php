bebd23e2151256cf45d3566bd1d514b8627d273c
malonezx <123611631+malonezx@users.noreply.github.com>
2023-11-01 02:00:16 -0300

    site testing for me

---
+++ upload.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_FILES["arquivo"])) {
+    $targetDir = "uploads/";
+    $targetFile = $targetDir . basename($_FILES["arquivo"]["name"]);
+
+    if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $targetFile)) {
+        // Envio bem-sucedido
+        http_response_code(200);
+    } else {
+        // Erro ao enviar o arquivo
+        http_response_code(500);
+    }
+} else {
+    // Requisição inválida
+    http_response_code(400);
+}
+?>
